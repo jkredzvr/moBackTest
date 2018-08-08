26788e02136bca73f75f2e797c0c1174f3b41b35
jchin <justinchin.bcchs@gmail.com>
2018-08-08 14:35:02 -0600

    added standard assets for 3rd person control mapped WASD movements to control sphere/world rotation

---
+++ Assets/FloatingObjs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObjs : MonoBehaviour {
+    public Rigidbody rb;
+    public float angleDiffUp;
+    public float angleThres;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        angleDiffUp = Vector3.Angle(transform.up, Vector3.up);
+
+        if (angleDiffUp > angleThres) {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+        }
+        else
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
+        }
+	}
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/SphereMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class SphereMovement : MonoBehaviour
+    {   
+        //Rate of rotation for input WASD
+        public float tiltAngle = .01f;
+        public float smooth = 5.0f;
+        private float tiltAroundZ = 0.0f;
+        private float tiltAroundX = 0.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            tiltAroundZ = tiltAroundZ % 360.0f;
+            tiltAroundX = tiltAroundX % 360.0f;
+            //Update Sphere rotation values
+            Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+            //Smooth lerp current rotation to target rotation 
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        }
+
+        public void move(Vector3 moveVector)
+        {
+            tiltAroundZ += moveVector.x * tiltAngle;
+
+            tiltAroundX -= moveVector.z * tiltAngle;
+        }
+
+        public void ChangeMovementSpeed(float newMovementSpeed)
+        {
+            tiltAngle = newMovementSpeed;
+        }
+    }
+}
